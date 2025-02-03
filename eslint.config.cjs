ac8f2af5315df2ddb587bff10c0fef806caeaade
weinni2000 <weinni2000@gmail.com>
2025-02-03 12:22:53 +0100

    migration hotel

--- eslint.config.cjs
+++ eslint.config.cjs
@@ -16,6 +16,7 @@ const config = [{
             openerp: "readonly",
             owl: "readonly",
             luxon: "readonly",
+            window: "readonly",
         },
 
         ecmaVersion: 2024,
